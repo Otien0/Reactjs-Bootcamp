4636d9cb7280e390b7a22878879027224bf7f061
mourice-oduor <otienomaurice364@gmail.com>
2020-07-08 21:45:36 -0400

    Added files/scripts

---
+++ Basics/React-Router/my-app/src/Blog.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom'
+
+class Blog extends Component {
+
+	render(){
+		return(
+			<div>
+				<h2>Blog</h2>
+				<ul>
+					<li><Link activeStyle={{color: 'red'}} to="/blog/123">Blog 1</Link></li>
+					<li><Link activeStyle={{color: 'red'}} to="/blog/234">Blog 2</Link></li>
+					<li><Link activeStyle={{color: 'red'}} to="/blog/456">Blog 3</Link></li>
+				</ul>
+				{this.props.children}
+			</div>
+		);
+	}
+
+}
+
+export default Blog;
