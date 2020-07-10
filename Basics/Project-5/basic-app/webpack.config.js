63279a5868c6e4253a2252910a2e964bdbe487d7
mourice-oduor <otienomaurice364@gmail.com>
2020-07-10 00:12:37 -0400

    Update webpack file

--- Basics/Project-5/basic-app/webpack.config.js
+++ Basics/Project-5/basic-app/webpack.config.js
@@ -24,3 +24,28 @@ module.exports = {
         ]
     }
 }
+
+// var path = require('path');
+// var webpack = require('webpack');
+// module.exports = {
+//     entry: './js/app.js',
+//     output: {
+//         path: path.resolve(__dirname, 'build'),
+//         filename: 'app.bundle.js'
+//     },
+//     module: {
+//         loaders: [
+//             {
+//                 test: /\.js$/,
+//                 loader: 'babel-loader',
+//                 query: {
+//                     presets: ['es2015']
+//                 }
+//             }
+//         ]
+//     },
+//     stats: {
+//         colors: true
+//     },
+//     devtool: 'source-map'
+// };
