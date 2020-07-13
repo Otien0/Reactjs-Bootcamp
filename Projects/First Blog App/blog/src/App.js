c7cdb2ec856b78b30fe96d0b00597651a6b1565d
mourice-oduor <otienomaurice364@gmail.com>
2020-07-13 01:44:45 -0400

    Update src/App.js

--- Projects/First Blog App/blog/src/App.js
+++ Projects/First Blog App/blog/src/App.js
@@ -1,151 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Header from './Header';
+import Container from './Container';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-          <div className="container">
-            <a className="navbar-brand" href="index.html">React_Blog</a>
-            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              Menu
-              <i className="fas fa-bars"></i>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="index.html">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="about.html">About</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="post.html">Sample Post</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="contact.html">Contact</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <header className="masthead my-img">
-          <div className="overlay"></div>
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-8 col-md-10 mx-auto">
-                <div className="site-heading">
-                  <h1>Clean React Blog</h1>
-                  <span className="subheading">A Blog Theme by Moryso Through Start Bootstrap Template</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </header>
-
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8 col-md-10 mx-auto">
-              <div className="post-preview">
-                <a href="post.html">
-                  <h2 className="post-title">
-                    Man must explore, and this is exploration at its greatest
-                </h2>
-                  <h3 className="post-subtitle">
-                    Problems look mighty small from 150 miles up
-                </h3>
-                </a>
-                <p className="post-meta">Posted by
-                <a href="/#"> Moryso </a>
-                on September 24, 2019</p>
-              </div>
-              <hr />
-              <div className="post-preview">
-                <a href="post.html">
-                  <h2 className="post-title">
-                    I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-                </h2>
-                </a>
-                <p className="post-meta">Posted by
-                <a href="/#"> Moryso </a>
-                on September 18, 2019</p>
-              </div>
-              <hr />
-              <div className="post-preview">
-                <a href="post.html">
-                  <h2 className="post-title">
-                    Science has not yet mastered prophecy
-                </h2>
-                  <h3 className="post-subtitle">
-                    We predict too much for the next year and yet far too little for the next ten.
-                </h3>
-                </a>
-                <p className="post-meta">Posted by
-               <a href="/#"> Moryso </a>
-               on August 24, 2019</p>
-              </div>
-              <hr />
-              <div className="post-preview">
-                <a href="post.html">
-                  <h2 className="post-title">
-                    Failure is not an option
-                </h2>
-                  <h3 className="post-subtitle">
-                    Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-                </h3>
-                </a>
-                <p className="post-meta">Posted by
-                <a href="/#"> Moryso </a>
-                on July 8, 2019</p>
-              </div>
-              <hr />
-              <div className="clearfix">
-                <a className="btn btn-primary float-right" href="/#">Older Posts &rarr;</a>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        <Navbar />
+        <Header />
         <hr />
-
-        <footer>
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-8 col-md-10 mx-auto">
-                <ul className="list-inline text-center">
-                  <li className="list-inline-item">
-                    <a href="/https://twitter.com/Morys0" target="_blank" rel="noopener noreferrer">
-                      <span className="fa-stack fa-lg">
-                        <i className="fas fa-circle fa-stack-2x"></i>
-                        <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                      </span>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="/https://www.facebook.com/morris.otieno.129" target="_blank" rel="noopener noreferrer">
-                      <span className="fa-stack fa-lg">
-                        <i className="fas fa-circle fa-stack-2x"></i>
-                        <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                      </span>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="/https://github.com/mourice-oduor" target="_blank" rel="noopener noreferrer">
-                      <span className="fa-stack fa-lg">
-                        <i className="fas fa-circle fa-stack-2x"></i>
-                        <i className="fab fa-github fa-stack-1x fa-inverse"></i>
-                      </span>
-                    </a>
-                  </li>
-                </ul>
-                <p className="copyright text-muted">Copyright &copy; Moryso's React_Blog 2020</p>
-              </div>
-            </div>
-          </div>
-        </footer>
+        <Container />
+        <hr />
+        <Footer />
 
       </div>
     );
