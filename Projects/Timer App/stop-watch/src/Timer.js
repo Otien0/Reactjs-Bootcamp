8adef67344f556c4c4052f981cfbd923645b9906
mourice-oduor <otienomaurice364@gmail.com>
2020-07-13 18:23:17 -0400

    Added Timer Component

---
+++ Projects/Timer App/stop-watch/src/Timer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+function Timer () {
+    return (
+        <div>
+            <p> You have been on this site since: </p> <br/>
+            <span> 55.98 </span>
+            <p> Seconds. </p>
+        </div>
+    );
+}
+
+export default Timer;
