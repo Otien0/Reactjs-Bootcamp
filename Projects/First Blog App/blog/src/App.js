9d548c7b354333c899b855af6e1057cc92aba14a
mourice-oduor <otienomaurice364@gmail.com>
2020-07-12 21:46:58 -0400

    Update src/App.js

--- Projects/First Blog App/blog/src/App.js
+++ Projects/First Blog App/blog/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
           <div className="container">
-            <a className="navbar-brand" href="index.html">Moryso_Blog</a>
+            <a className="navbar-brand" href="index.html">React_Blog</a>
             <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
               Menu
               <i className="fas fa-bars"></i>
@@ -32,7 +32,7 @@ class App extends Component {
           </div>
         </nav>
 
-        <header className="masthead">
+        <header className="masthead my-img">
           <div className="overlay"></div>
           <div className="container">
             <div className="row">
