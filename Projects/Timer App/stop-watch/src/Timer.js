1ecc08dc0eca162e16667d838c3f345601b9f22c
mourice-oduor <otienomaurice364@gmail.com>
2020-07-13 21:57:47 -0400

    Update Timer Component

--- Projects/Timer App/stop-watch/src/Timer.js
+++ Projects/Timer App/stop-watch/src/Timer.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 
-function Timer () {
-    return (
-        <div>
-            <p> You have been on this site since: </p> <br/>
-            <span> 55.98 </span>
-            <p> Seconds. </p>
-        </div>
-    );
+
+
+class Timer extends Component {
+
+    componentDidMount() {
+        this.timer = setInterval(this.ticker, 1000)
+    }
+
+    ticker() {
+        this.setState({ clock: new Date() - this.props.start })
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            clock: 0
+        };
+        this.ticker = this.ticker.bind(this);
+    }
+
+    render() {
+        var clock = Math.round(this.state.clock / 1000);
+        return (
+            <div>
+                <p> You have been on this site since: </p> <br />
+                <span> {clock} </span>
+                <p> Seconds. </p>
+            </div>
+        );
+    }
+
+
 }
 
 export default Timer;
