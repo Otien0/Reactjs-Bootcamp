6b65d45053b9ec0f74bc9490a3f21547daf0de17
mourice-oduor <otienomaurice364@gmail.com>
2020-07-15 05:10:45 -0400

    Added Coursesales Component

---
+++ Projects/Course-Sales App/course-purchase/src/Coursesales.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Coursesales extends Component {
+    render(){
+        console.log(this.props.items);
+        
+        return(
+            <div>
+                I am from Coursesales Page
+            </div>
+        );
+    }
+}
+
+export default Coursesales;
