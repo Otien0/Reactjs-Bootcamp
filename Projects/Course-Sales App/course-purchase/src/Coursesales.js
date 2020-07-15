15255f5a62a48062ba054ba4e098911dd1c72ce2
mourice-oduor <otienomaurice364@gmail.com>
2020-07-15 05:51:59 -0400

    Update Coursesales Component

--- Projects/Course-Sales App/course-purchase/src/Coursesales.js
+++ Projects/Course-Sales App/course-purchase/src/Coursesales.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 
 class Coursesales extends Component {
-    render(){
+    sumPrice(price) {
+        this.setState({ total: this.state.total + price });
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            total: 0
+        };
+        this.sumPrice = this.sumPrice.bind(this);
+    }
+
+    render() {
         console.log(this.props.items);
-        
-        return(
+        var courses = this.props.items.map((item, i) => {
+            return <Course name={item.name} price={item.price}
+                key={1} sumPrice={this.sumPrice} active={item.active} />
+        });
+
+        return (
             <div>
-                I am from Coursesales Page
+                <h1>You can buy courses: </h1>
+
+                <div id="courses">
+                    {courses}
+                    <p id="total"> Total: <strong>{this.state.total}</strong> </p>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Course extends Component {
+    render() {
+        return (
+            <div>
+
             </div>
         );
     }
