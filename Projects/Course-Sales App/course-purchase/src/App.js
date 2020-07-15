fa5cd875cfc50dd2b9dc2c66968706648852fbbe
mourice-oduor <otienomaurice364@gmail.com>
2020-07-15 05:09:42 -0400

    Update App.js

--- Projects/Course-Sales App/course-purchase/src/App.js
+++ Projects/Course-Sales App/course-purchase/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Coursesales from './Coursesales';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+class App extends Component {
+  render() {
+
+    var courses = [
+      { name: 'Complete Full-Stack Java-Spring Course', price: 199 },
+      { name: 'Complete Front-end Developer Course', price: 299 },
+      { name: 'Complete Pentesting Course', price: 160 },
+      { name: 'Complete Full-Stack Python-Django Course', price: 255 }
+    ];
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to Course-sales App</h2>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+        <Coursesales items={courses} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
