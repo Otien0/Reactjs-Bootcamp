61d13f75ce9016dd8b6138c51d412c13e6683883
mourice-oduor <otienomaurice364@gmail.com>
2020-07-22 04:12:36 -0400

    Added a Quiz.js Component

---
+++ Projects/Random-Game/random-number/src/Quiz.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Quiz extends Component {
+    render(){
+        return(
+            <div>
+                Hi I am from Quiz
+            </div>
+        );
+    }
+}
+
+export default Quiz;
