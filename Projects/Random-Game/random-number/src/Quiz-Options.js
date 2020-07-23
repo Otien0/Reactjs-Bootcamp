dea5eafdd5c27262bb0664ed0ff446870772e02f
mourice-oduor <otienomaurice364@gmail.com>
2020-07-23 04:44:18 -0400

    Added Quiz-Options Component

---
+++ Projects/Random-Game/random-number/src/Quiz-Options.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Quiz_Options extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Quiz_Options;
