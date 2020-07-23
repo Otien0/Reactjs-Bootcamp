d09340575bf5b275a502fe3d89181522879591bf
mourice-oduor <otienomaurice364@gmail.com>
2020-07-23 06:16:10 -0400

    Update Quiz_Options.js Component

--- Projects/Random-Game/random-number/src/Quiz_Options.js
+++ Projects/Random-Game/random-number/src/Quiz_Options.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
-class Quiz_Options extends Component{
-    render(){
-        return(
-            <div>
 
+class Quiz_Options extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+        this.callParentCheckOptions = this.callParentCheckOptions.bind(this);
+    }
+    callParentCheckOptions() {
+        this.props.checkResults(this.props.option);
+    }
+    render() {
+        return (
+            <div className="fields animated zoomIn" onClick={this.callParentCheckOptions}>
+                <div className="field-block">{this.props.option}</div>
             </div>
         );
     }
