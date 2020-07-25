147bf192fa608603705e94cab75351fe08088377
mourice-oduor <otienomaurice364@gmail.com>
2020-07-25 05:32:00 -0400

    Added EasyABC Component

---
+++ Projects/ABC-Alphabet Game/alphabet-game/src/EasyABC.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EasyABC extends Component {
+    render(){
+        return(
+            <div>
+                Hi I'm from Easy ABC
+            </div>
+        );
+    }
+}
+
+export default EasyABC;
