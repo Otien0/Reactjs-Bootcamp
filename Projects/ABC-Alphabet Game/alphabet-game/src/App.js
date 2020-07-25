683e7b969db40b4861a618515e539be68da81934
mourice-oduor <otienomaurice364@gmail.com>
2020-07-25 05:32:53 -0400

    Update App.js

--- Projects/ABC-Alphabet Game/alphabet-game/src/App.js
+++ Projects/ABC-Alphabet Game/alphabet-game/src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EasyABC from './EasyABC';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Hello! Welcome to Easy ABC.. Alphabet-Game, with React.js</h2>
+
+        <EasyABC />
       </header>
     </div>
   );
