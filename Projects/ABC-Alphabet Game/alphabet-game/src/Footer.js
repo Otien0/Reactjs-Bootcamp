a36578133423db64350b730a6c2b128b1437acf5
mourice-oduor <otienomaurice364@gmail.com>
2020-07-31 22:05:53 -0400

    Added Footer.js Component

---
+++ Projects/ABC-Alphabet Game/alphabet-game/src/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render(){
+        return(
+            <div className="footer">
+                
+                <footer>
+                    <p> Copyright &copy;2020. All Rights Reserved. </p>
+                    <p> Designed by @moryso |  <strong> Terms of Service  |</strong>
+                        <strong>Privacy Policy</strong>
+                    </p>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
