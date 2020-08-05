167a7ccc2e26e7d1db008567da854514ff20b302
mourice-oduor <otienomaurice364@gmail.com>
2020-08-04 20:54:46 -0400

    Added Project.js Component

---
+++ Projects/React Resume/resume/src/Project.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Project extends Component {
+	render() {
+		return (
+			<div className="item">
+				<span className="project-title">
+					<a href={this.props.item.url} target="_blank" rel="noopener noreferrer">{this.props.item.name}</a>
+				</span> - <span className="project-tagline">{this.props.item.detail}</span>
+			</div>
+		);
+	}
+}
+
+export default Project;
