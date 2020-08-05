1f5496d2aa15166f00c31c1e0060734174c4e280
mourice-oduor <otienomaurice364@gmail.com>
2020-08-04 20:53:40 -0400

    Added Experience.js Component

---
+++ Projects/React Resume/resume/src/Experience.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+
+class Experience extends Component {
+
+	render() {
+		return (
+			<div className="item">
+                <div className="meta">
+                    <div className="upper-row">
+                        <h3 className="job-title">{this.props.item.title}</h3>
+                        <div className="time">{this.props.item.period}</div>
+                    </div>
+                    <div className="company">
+                        <a href={this.props.item.companyURL} target="_blank" rel="noopener noreferrer">{this.props.item.company}</a>
+                    </div>
+                </div>
+                <div className="details">
+                    <p>{this.props.item.details}</p>
+                </div>
+            </div>
+		);
+	}
+}
+
+export default Experience;
