f582a1dfeccd6c27d74b624134c9bf4e3fdff40b
mourice-oduor <otienomaurice364@gmail.com>
2020-08-23 18:44:18 -0400

    Added Submit.js Component

---
+++ Projects/Recipe App/food-recipes/src/Submit.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Submit extends Component {
+    render() {
+        return(
+            <div>
+                <h3>Submit</h3>
+            </div>
+        );
+    }
+}
+
+export default Submit;
