d901db148792f1c3f218d1c4ae50d3102e0b37a5
mourice-oduor <otienomaurice364@gmail.com>
2020-08-23 18:40:52 -0400

    Update App.js file

--- Projects/Recipe App/food-recipes/src/App.js
+++ Projects/Recipe App/food-recipes/src/App.js
@@ -5,17 +5,17 @@ import { NavLink } from 'react-router-dom';
 function App() {
   return (
     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a className="navbar-brand" href="/#">Food-Recipes</a>
+      <NavLink className="navbar-brand" to="/#">Food-Recipes</NavLink>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarCollapse">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="/#">Home <span className="sr-only">(current)</span></a>
+            <NavLink className="nav-link" to="/#">Home <span className="sr-only">(current)</span></NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/#">Submit a recipe</a>
+            <NavLink className="nav-link" to="/#">Submit recipe</NavLink>
           </li>
         </ul>
       </div>
