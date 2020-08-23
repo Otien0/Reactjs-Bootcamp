49c9609bad8f16510e3b2753d863bedfdca87f6a
mourice-oduor <otienomaurice364@gmail.com>
2020-08-23 18:09:18 -0400

    Update index.js file

--- Projects/Recipe App/food-recipes/src/index.js
+++ Projects/Recipe App/food-recipes/src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Home from './Home';
+import Submit from './Submit';
 import * as serviceWorker from './serviceWorker';
+import { Router, Route, BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <div>
+        <Route path='/' component={App} />
+        <Route path='/home' component={Home} />
+        <Route path='/submit' component={Submit} />
+    </div>
+  </BrowserRouter>,
   document.getElementById('root')
 );
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
