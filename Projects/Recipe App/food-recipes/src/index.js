20b3ee582f017c7783246d05f84b2529636c42da
mourice-oduor <otienomaurice364@gmail.com>
2020-08-25 23:53:05 -0400

    Update index.js file

--- Projects/Recipe App/food-recipes/src/index.js
+++ Projects/Recipe App/food-recipes/src/index.js
@@ -4,15 +4,37 @@ import './index.css';
 import App from './App';
 import Home from './Home';
 import Submit from './Submit';
+import { NavLink } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import { Router, Route, BrowserRouter } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <BrowserRouter>
-    <div>
-        <Route path='/' component={App} />
-        <Route path='/home' component={Home} />
-        <Route path='/submit' component={Submit} />
+    <div className="container">
+
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <NavLink className="navbar-brand" to="/#">Food-Recipes</NavLink>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <NavLink exact activeClassName="activeNav" to="/">Home <span className="sr-only">(current)</span></NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink activeClassName="activeNav" to="/submit">Submit a Recipe</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <Route path='/' component={Home} />
+      <Route path='/submit' component={Submit} history={history} />
+
     </div>
   </BrowserRouter>,
   document.getElementById('root')
