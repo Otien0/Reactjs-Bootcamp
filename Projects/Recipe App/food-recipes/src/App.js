d1be74b544e063ad7c24710e6e875a1c34b9179f
mourice-oduor <otienomaurice364@gmail.com>
2020-08-27 21:38:44 -0400

    Update Components

--- Projects/Recipe App/food-recipes/src/App.js
+++ Projects/Recipe App/food-recipes/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import { NavLink } from 'react-router-dom';
 
 function App() {
   return (
--- Projects/Recipe App/food-recipes/src/Submit.js
+++ Projects/Recipe App/food-recipes/src/Submit.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 
 class Submit extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {};
-        this.submitRecipe = this.submitRecipe.bind(this);
-    }
-
-    submitRecipe() {
-        console.log('button clicked');
-        this.props.history.push('/');
-    }
-
     render() {
         return (
-            <div>
-                <h2>Submit</h2>
-                <button onClick={this.submitRecipe}>Submit a Recipe</button>
+            <div className="row">
+                <div className="col-xs-12 col-sm-12">
+                    <h2>Submit</h2>
+                    <form>
+                        <div className="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+
+                        <div className="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" />
+                        </div>
+
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
         );
     }
