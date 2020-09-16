0c38eeb92b10dc0ae5b71f9968b4babaf6756e38
mourice-oduor <otienomaurice364@gmail.com>
2020-09-15 22:12:30 -0400

    Update Ingredients.js Component

--- Projects/Recipe App/food-recipes/src/Ingredients.js
+++ Projects/Recipe App/food-recipes/src/Ingredients.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 
 class Ingredients extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+        this.addIngredients = this.addIngredients.bind(this);
+    }
+
+    addIngredients() {
+        console.log('Ingredient Added');
+        console.log(this.quantity.value, this.ingredient.value);
+        this.props.addIngredient(this.quantity.value, this.ingredient.value);
+
+        this.quantity.value = "";
+        this.ingredient.value = "";
+    }
+
     render() {
         return (
             <div className="form-inline form-group">
                 <label htmlFor="quantity">Quantity</label>
-                <input type="text" className="form-control" id="quantity" placeholder="Quantity" aria-describedby="quantitylHelp" />
-
+                <input type="text"
+                    className="form-control"
+                    id="quantity"
+                    ref={(input) => { this.quantity = input; }}
+                    placeholder="Quantity" />
                 <label htmlFor="ingredient">Ingredient</label>
-                <input type="text" className="form-control" id="ingredient" placeholder="Ingredient" aria-describedby="ingredientHelp" />
-
-                <button type="submit" className="btn btn-primary">Add</button>
+                <input type="text"
+                    className="form-control"
+                    ref={(input) => { this.ingredient = input; }}
+                    id="ingredient"
+                    placeholder="Ingredient" />
+                <button type="button" onClick={this.addIngredients} className="btn btn-info">Add</button>
             </div>
-    );
+        );
     }
 }
 
