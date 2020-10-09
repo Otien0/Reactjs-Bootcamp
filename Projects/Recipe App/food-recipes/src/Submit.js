ef0d620b5bb82631b42a54155957d7e4c46483e5
mourice-oduor <otienomaurice364@gmail.com>
2020-10-08 23:40:44 -0400

    Update Submit.js Component

--- Projects/Recipe App/food-recipes/src/Submit.js
+++ Projects/Recipe App/food-recipes/src/Submit.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
+
 import Ingredients from './Ingredients';
 import IngredientList from './IngredientList';
-// import { browserHistory } from 'react-router';
-import { withRouter } from "react-router";
+import { browserHistory } from 'react-router';
+
+const CLOUDINARY_UPLOAD_PRESET = 'csxglbua';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/ds7zkewzh/upload';
+
 
 class Submit extends Component {
 
     constructor(props) {
         super(props);
-
 
         this.state = {
             recipies: JSON.parse(localStorage.getItem('recipies')) || [],
@@ -16,9 +22,37 @@ class Submit extends Component {
                 name: "New Recipie",
                 description: "Description",
                 ingredients: []
-            }
+            },
+            uploadedFileCloudinaryUrl: ''
         };
         this.submitRecipe = this.submitRecipe.bind(this);
+        this.onImageDrop = this.onImageDrop.bind(this);
+    }
+
+    onImageDrop(files) {
+        this.setState({
+            uploadedFile: files[0]
+        });
+
+        this.handleImageUpload(files[0]);
+    }
+
+    handleImageUpload(file) {
+        let upload = request.post(CLOUDINARY_UPLOAD_URL)
+            .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+            .field('file', file);
+
+        upload.end((err, response) => {
+            if (err) {
+                console.error(err);
+            }
+
+            if (response.body.secure_url !== '') {
+                this.setState({
+                    uploadedFileCloudinaryUrl: response.body.secure_url
+                });
+            }
+        });
     }
 
     submitRecipe() {
@@ -28,8 +62,8 @@ class Submit extends Component {
 
         newRecipie.name = this.name.value;
         newRecipie.description = this.description.value;
+        newRecipie.image = this.state.uploadedFileCloudinaryUrl;
 
-        //Update newRecipie
         this.setState({ newRecipie });
 
         let recipies = this.state.recipies;
@@ -38,10 +72,9 @@ class Submit extends Component {
         this.setState({ recipies });
         localStorage.setItem('recipies', JSON.stringify(recipies));
         console.log(recipies);
-        this.props.history.push('/');
-    }
+        browserHistory.push('/');
 
-    // const HomeWitRouter = withRouter(Home);
+    }
 
     addIngredient(quantity, ingredient) {
         console.log("Add Ingredients in Submit js", quantity, ingredient);
@@ -55,32 +88,42 @@ class Submit extends Component {
         return (
             <div className="row">
                 <div className="col-xs-12 col-sm-12">
-                    <h2>Submit</h2>
+                    <h1>Submit</h1>
                     <form>
+                        <Dropzone
+                            multiple={false}
+                            accept="image/*"
+                            onDrop={this.onImageDrop}>
+                            <p>Drop an image or click to select a file to upload.</p>
+                        </Dropzone>
+                        <div>
+                            {this.state.uploadedFileCloudinaryUrl === '' ? null :
+                                <div>
+                                    <p>{this.state.uploadedFile.name}</p>
+                                    <img alt={this.state.uploadedFile.name} src={this.state.uploadedFileCloudinaryUrl} />
+                                </div>}
+                        </div>
                         <div className="form-group">
                             <label htmlFor="name">Name</label>
                             <input type="text"
                                 ref={(input) => { this.name = input; }}
                                 className="form-control"
                                 id="name"
-                                placeholder="Enter the name of the recipe"
-                                aria-describedby="nameHelp" />
+                                placeholder="Enter the name of the recipie" />
                         </div>
-
                         <div className="form-group">
                             <label htmlFor="description">Description</label>
-                            <textarea className="form-control" id="description" placeholder="Enter a brief description" aria-describedby="descriptionlHelp" />
+                            <textarea className="form-control"
+                                id="description"
+                                ref={(input) => { this.description = input; }}
+                                placeholder="Enter a brief description" />
                         </div>
 
                         <IngredientList recipie={this.state.newRecipie} />
-
                         <Ingredients addIngredient={(quantity, ingredient) => { this.addIngredient(quantity, ingredient) }} />
-
-                        <button type="button" onClick={this.submitRecipe} className="btn btn-dark">Submit</button>
-
+                        <button type="button" onClick={this.submitRecipe} className="btn btn-default">Submit</button>
                     </form>
                 </div>
-
             </div>
         );
     }
