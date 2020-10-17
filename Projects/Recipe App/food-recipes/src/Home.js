e040fcfef68a2529ca55891fdce798410f72877f
mourice-oduor <otienomaurice364@gmail.com>
2020-10-17 05:19:32 -0400

    Update Src/Home.js Component

--- Projects/Recipe App/food-recipes/src/Home.js
+++ Projects/Recipe App/food-recipes/src/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import IngredientList from './IngredientList';
+// import Footer from './Footer';
 
 class Home extends Component {
 
@@ -18,13 +19,16 @@ class Home extends Component {
         // eslint-disable-next-line array-callback-return
         this.state.recipies.map((recipie, i) => {
             resultsArray.push(
-                <div className="col-sm-6 col-md-4">
-                    <div className="thumbnail">
-                        <img src={recipie.image} alt={recipie.name} />
-                        <div className="caption">
-                            <h3>{recipie.name}</h3>
-                            <p>{recipie.description}</p>
+                <div className="col-sm-4 col-md-4">
+                    <div className="card">
+
+                        <div className="card-body">
+                            <h3 className="card-title">{recipie.name}</h3>
+                            <img src={recipie.image} alt={recipie.name} className="card-img-top" />
+                            <p className="card-text">{recipie.description}</p>
                             <IngredientList recipie={recipie} />
+
+                            <a href="/#" className="btn btn-primary">More Info</a>
                         </div>
                     </div>
                 </div>
@@ -37,8 +41,10 @@ class Home extends Component {
     render() {
         return (
             <div className="row">
-                <h1>Home</h1>
+                <h1>Hello, welcome to my recipe app made with react-js</h1>
                 {this.displayRecipies()}
+
+                {/* <Footer /> */}
             </div>
         );
     }
