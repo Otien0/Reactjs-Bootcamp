f7e85b8ded1eab5453ce6cddf38bd464c04630d2
mourice-oduor <otienomaurice364@gmail.com>
2020-10-22 04:36:14 -0400

    Added Survey.js Component

---
+++ Projects/Survey App/src/Survey.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Survey extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Survey;
