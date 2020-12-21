954c7bd2135af7a8c5bdd737b2304b0273ed7285
mourice-oduor <otienomaurice364@gmail.com>
2020-12-20 20:28:36 -0500

    Update App.js

--- Projects/Firebase Login App/firebase-login/src/App.js
+++ Projects/Firebase Login App/firebase-login/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Authenticate from './Authenticate';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to React Login with Firebase App</h1>
       </header>
+      <Authenticate />
     </div>
   );
 }
