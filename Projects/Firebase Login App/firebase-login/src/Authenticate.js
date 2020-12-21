1231709e22b8840ae85f63dfbe1a4d6715637897
mourice-oduor <otienomaurice364@gmail.com>
2020-12-20 20:30:08 -0500

    Added Authenticate.js Component

---
+++ Projects/Firebase Login App/firebase-login/src/Authenticate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Authenticate extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Hi I am from Authenticate Component</h1>
+            </div>
+        );
+    }
+}
+
+export default  Authenticate;
+
