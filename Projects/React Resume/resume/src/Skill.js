c9393a0e4a4c8ce2e6e5b42ffa037a8717bc69bc
mourice-oduor <otienomaurice364@gmail.com>
2022-04-23 07:15:13 +0300

    update script contents

--- Projects/React Resume/resume/src/Skill.js
+++ Projects/React Resume/resume/src/Skill.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Skill extends Component {
 	render() {
 		return (
-			<div className="item">
+            <div className="item">
                 <h3 className="level-title">{this.props.item.name}</h3>
                 <div className="level-bar">
                     <div className="level-bar-inner" data-level={this.props.item.percent}></div>                                      
