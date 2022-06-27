e16c2dac9118119cfb5ffec401827c04ad9513ec
mourice-oduor <otienomaurice364@gmail.com>
2022-06-27 23:54:03 +0300

    mui buttons

--- Material-UI/muidemo/src/pages/Buttons.js
+++ Material-UI/muidemo/src/pages/Buttons.js
@@ -1,8 +1,31 @@
+import { Button, ButtonGroup } from '@mui/material'
+import { Save, Delete } from '@mui/icons-material'
 import React from 'react'
 
 function Buttons() {
   return (
-    <div>Buttons</div>
+    <div className='m-5'>
+        <div>
+            <h2> MUI Buttons</h2>
+            <ButtonGroup>
+                <Button className='mt-3 mr-3'
+                    startIcon = {<Save/>}
+                    size='large'
+                    variant='contained'
+                    color='primary'>
+                    Save
+                </Button>
+
+                <Button className='mt-3 ml-3'
+                    startIcon = {<Delete/>}
+                    size='large'
+                    variant='contained'
+                    color='secondary'>
+                    Delete
+                </Button>
+            </ButtonGroup>
+        </div>
+    </div>
   )
 }
 
