6a07f4ecbfdd897b06f948a1d0feae375d5ebcb8
mourice-oduor <otienomaurice364@gmail.com>
2022-06-28 12:45:11 +0300

    update app.js

--- Material-UI/muidemo2/src/App.js
+++ Material-UI/muidemo2/src/App.js
@@ -1,21 +1,33 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
 import Tour from "./pages/Tour";
+import { Container, Grid } from '@mui/material';
 // import SearchAppBar from "./components/AppBar";
 
 function App() {
   return (
-    <BrowserRouter>
-      {/* <SearchAppBar /> */}
-      <div className="App">
-        <Routes>
-            <Route path='/' element={<Home />}/>
-            {/* <Route path="/:id" element={<Tour />} /> */}
-            <Route path="/tour" element={<Tour />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className='App'>
+      <Container>
+        <Grid container spacing={5}>
+          <Tour/>
+          <Tour/>
+          <Tour/>
+          <Tour/>
+        </Grid>
+      </Container>
+
+    </div>
+    // <BrowserRouter>
+    //   {/* <SearchAppBar /> */}
+    //   <div className="App">
+    //     <Routes>
+    //         <Route path='/' element={<Home />}/>
+    //         {/* <Route path="/:id" element={<Tour />} /> */}
+    //         <Route path="/tour" element={<Tour />} />
+    //     </Routes>
+    //   </div>
+    // </BrowserRouter>
   );
 }
 
