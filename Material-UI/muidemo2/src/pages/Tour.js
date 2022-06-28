77648c6a24c3628555dfe7ab427b78aaea4fc09f
mourice-oduor <otienomaurice364@gmail.com>
2022-06-28 11:15:58 +0300

    update tour page

--- Material-UI/muidemo2/src/pages/Tour.js
+++ Material-UI/muidemo2/src/pages/Tour.js
@@ -1,8 +1,21 @@
+import { Container, Grid, Paper, Typography } from '@mui/material'
 import React from 'react'
 
 function Tour() {
   return (
-    <div>Tour</div>
+    <Container>
+        <Grid item xs={3}>
+            <Paper elevation={3}>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVtylpGnRgeOovFNYjjl_JcXcshNDUvSpheQ&usqp=CAU" 
+                    alt=""
+                    className='img m-5' />
+            </Paper>
+
+            <Typography>
+                The beauty of Earth
+            </Typography>
+        </Grid>
+    </Container>
   )
 }
 
