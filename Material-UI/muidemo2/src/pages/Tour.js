2369b6671c1d869697fdfdcecd16bf78308bdb77
mourice-oduor <otienomaurice364@gmail.com>
2022-06-28 11:39:32 +0300

    update tour page

--- Material-UI/muidemo2/src/pages/Tour.js
+++ Material-UI/muidemo2/src/pages/Tour.js
@@ -1,4 +1,5 @@
-import { Container, Grid, Paper, Typography } from '@mui/material'
+import { Container, Grid, Paper, Typography, Box } from '@mui/material'
+import { AccessTime } from '@mui/icons-material'
 import React from 'react'
 
 function Tour() {
@@ -8,12 +9,24 @@ function Tour() {
             <Paper elevation={3}>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVtylpGnRgeOovFNYjjl_JcXcshNDUvSpheQ&usqp=CAU" 
                     alt=""
-                    className='img m-5' />
+                    className=' m-5' />
             </Paper>
 
-            <Typography>
-                The beauty of Earth
-            </Typography>
+            <Box paddingX={1}>
+                <Typography variant='subtitle1' component='h2'>
+                    The beauty of Earth
+                </Typography>
+
+                <Box
+                    sx={{ display: 'flex',
+                        alignItems: 'center',}}
+                >
+                    <AccessTime/>
+                    <Typography variant='body2' component='p'>
+                        5 hours
+                    </Typography>
+                </Box>
+            </Box>
         </Grid>
     </Container>
   )
